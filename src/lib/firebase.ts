ca0c4e7d659362c662b111255f4353c33b268e37
Chiranjeev <chiatbits@gmail.com>
2026-03-06 20:35:44 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/una

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { initializeApp, getApps, getApp } from 'firebase/app'
+import { initializeApp, getApps } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
@@ -13,7 +13,7 @@ const firebaseConfig = {
 
 const app = getApps().length === 0 
   ? initializeApp(firebaseConfig) 
-  : getApp()
+  : getApps()[0]
 
 export const db = getFirestore(app)
 export const auth = getAuth(app)
