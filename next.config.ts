d4c756e87e63e8030c437b2159d7d6ce76685e5f
Chiranjeev <chiatbits@gmail.com>
2026-03-18 00:05:10 +0000

    NEXT_PUBLIC_SUPABASE_URL=https://ryufigjgfdbnfnzixgla.supabase.co NEXT_P

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: 'https://ryufigjgfdbnfnzixgla.supabase.co',
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5dWZpZ2pnZmRibmZueml4Z2xhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM3Mzg1MTUsImV4cCI6MjA4OTMxNDUxNX0.K_05eB2Vzby8MevaLrLx8zR88CmnB8dFxm27FtTrseE',
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
